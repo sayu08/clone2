873be8fb2ab8bf91ec7a7d125338663ae4e3a3ce
unknown <sayalib87@gmail.com>
2022-04-08 13:24:31 +0530

    Done all

---
+++ Demo2.java
@@ -0,0 +1,12 @@
+package github;
+
+public class Demo2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Demo2 class");
+		
+
+	}
+
+}
---
+++ Demo3.java
@@ -0,0 +1,10 @@
+package github;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		System.out.println("sayali bhagat");
+
+	}
+
+}
